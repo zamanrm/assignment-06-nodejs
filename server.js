ff238e32b7ee93e6085f500b9b2193f26554a670
zamanrm <112308046+zamanrm@users.noreply.github.com>
2026-09-10 02:04:39 +0600

    Add Home Route

--- server.js
+++ server.js
@@ -5,7 +5,25 @@ const PORT = 5000;
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
 
-  res.end("<h1>HTTP Server is Running</h1>");
+  // Home Route
+  if (req.url === "/") {
+    res.end("<h1>Home Page</h1>");
+  }
+
+  // Blog Route
+  else if (req.url === "/blog") {
+    res.end("<h1>Blog Page</h1>");
+  }
+
+  // Contact Route
+  else if (req.url === "/contact") {
+    res.end("<h1>Contact Page</h1>");
+  }
+
+  // About Route
+  else if (req.url === "/about") {
+    res.end("<h1>About Page</h1>");
+  }
 });
 
 server.listen(PORT, () => {
